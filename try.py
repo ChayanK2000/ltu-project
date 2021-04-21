1203c8b00724b9c1ba5cab47e11216123ea203bf
ChayanK2000 <chayan4bkn@gmail.com>
2021-04-21 06:04:14 +0530

    voila!

--- try.py
+++ try.py
@@ -1,19 +1,24 @@
 import json
-f = open('annotated.json', 'r')
+f = open('annotated_english.json', 'r')
 data = json.load(f)
 relPronList = ["who", "whom", "which", "that",
                "those", "whose", "where", "when", "why", "what"]
 for j in data:
 #for every sentence in data
-    print("sentence",j)
+    print("\nSentence ", j, ":")
+    print(data[j]['sentence'])
     for i in data[j]:
-        #for every word(by index starting from 1) in sentence
+        #for every word(by index starting from 1) in sentence. it also has the key "sentence"
+        if i == "sentence":
+            continue
         if data[j][i]['deprel'] == 'acl:relcl':
             pronoun_present = False
             # print(data[j][i]['id'], data[j][i]['text'], data[j][i]['head'], data[j][str(data[j][i]['head'])]['text'])
             headID = data[j][i]['head']
             currentID = data[j][i]['id']
             for k in data[j]:
+                if k == 'sentence':
+                    continue
                 if (data[j][k]['head'] == currentID):
                     if (data[j][k]['text'].lower() in relPronList):
                         print("REL PRONOUN: \"" +
