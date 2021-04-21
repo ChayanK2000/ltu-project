ecf6df090216d2f20b6fd3e1d14054450b0afca7
ChayanK2000 <chayan4bkn@gmail.com>
2021-04-21 08:16:23 +0530

    done

--- try.py
+++ try.py
@@ -1,11 +1,14 @@
 import json
-f = open('annotated_hindi.json', 'r')
+f = open('annotated_english.json', 'r')
 data = json.load(f)
-# relPronList = ["who", "whom", "which", "that",
-#                "those", "whose", "where", "when", "why", "what"]
+relPronList = ["who", "whom", "which", "that",
+               "those", "whose", "where", "when", "why", "what"]
 
-relPronList = ['जो', 'जोकि', 'जहाँ', 'जिधर', 'जितना', 'जितने', 'जैसा', 'जैसे', 'जिसको', 'जिसके',
-               'जिस', 'जिसे', 'जिससे', 'जिसकी', 'जिसका', 'जिसने', 'जिन्हें', 'जिन्होंने', 'जिसमें', 'जिनमें', 'जिनकी', 'जब']
+# relPronList = ['जो', 'जोकि', 'जहाँ', 'जिधर', 'जितना', 'जितने', 'जैसा', 'जैसे', 'जिसको', 'जिसके',
+#                'जिस', 'जिसे', 'जिससे', 'जिसकी', 'जिसका', 'जिसने', 'जिन्हें', 'जिन्होंने', 'जिसमें', 'जिनमें', 'जिनकी', 'जब']
+nom_rel = 0
+rel_nom = 0
+red_rel = 0
 for j in data:
 #for every sentence in data
     print("\nSentence ", j, ":")
@@ -28,8 +31,18 @@ for j in data:
                               data[j][k]['text']+"\" with ID: ", data[j][k]['id'])
                         print(
                             "REFERNT: \"" + data[j][str(data[j][i]['head'])]['text'] + "\" with ID :", headID)
+                        if data[j][k]['id'] > headID:
+                            nom_rel += 1
+                        else:
+                            rel_nom += 1
                         pronoun_present = True
             if pronoun_present == False:
                 print("no pronoun")
                 print(
                     "REFERNT: \"" + data[j][str(data[j][i]['head'])]['text'] + "\" with ID :", headID)
+                red_rel += 1
+
+print("==================================")
+print("Nominal then relative pronoun: ", nom_rel)
+print("Relative pronoun then nom: ", rel_nom)
+print("instances of reduced relativization: ",red_rel)
